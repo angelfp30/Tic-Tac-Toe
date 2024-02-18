ed4a6375d27fdf8c52d1f3e013c2a31b387c3b0f
angelfp30 <156565466+angelfp30@users.noreply.github.com>
2024-02-18 15:37:16 -0500

    The game works and markers display into the gameboard, every bug has been fixed but we dont know when a player has won

--- script.js
+++ script.js
@@ -10,10 +10,24 @@ const Gameboard = (() =>{
             boardHTML+= `<div class="square" id="square-${index}">${square}</div>` 
         })
         document.querySelector(".gameboard").innerHTML = boardHTML;
+        
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.addEventListener("click", Game.handleClick);
+        });
     }
+
+    const update = (index, value) => {
+        gameboard[index] = value;
+        render();
+    }
+
+    const getGameboard = () => gameboard ;
 
     return {
         render,
+        update,
+        getGameboard,
     }
 })();
 
@@ -37,10 +51,27 @@ const Game = (() =>{
         currentPlayerIndex = 0;
         gameOver = false;
         Gameboard.render();
+
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.addEventListener("click", handleClick);
+        });
+    }
+
+    const handleClick = (event) => {
+        let index = parseInt(event.target.id.split("-")[1]);
+
+        if (Gameboard.getGameboard()[index] !== ""){
+            return;
+        }
+
+        Gameboard.update(index, players[currentPlayerIndex].mark);
+        currentPlayerIndex =  currentPlayerIndex === 0 ? 1 : 0;
     }
 
     return {
         start,
+        handleClick,
     }
 })();
 
